94044a6d78710bdb6c068dd7aeab3f91b9e1e1d5
Minjun Xu <minjunxu@andrew.cmu.edu>
2019-03-31 11:37:20 -0400

    some initial files for camera

--- src/camera/Camera.h
+++ src/camera/Camera.h
@@ -9,9 +9,13 @@ public:
     ~Camera();
 
     YsMatrix4x4 getCameraMat();
-    void changeOrientation();
+    void changeOrientation(int angle);
+    void changePosition(YsVec3 pos);
+    void resetMat();
 private:
+    const int distance = 10;
     YsMatrix4x4 cameraMat;
+    int nowAngle = 0;
 };
 
 #endif
