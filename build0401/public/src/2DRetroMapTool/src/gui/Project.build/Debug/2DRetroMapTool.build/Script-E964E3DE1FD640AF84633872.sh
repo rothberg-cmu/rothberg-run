51bbf81d4c6c6249826adf21736c55dab6013318
jingzhao3200 <jingzhao3200@users.noreply.github.com>
2019-04-12 12:28:14 -0400

    Add color to cartoon boy

---
+++ build0401/public/src/2DRetroMapTool/src/gui/Project.build/Debug/2DRetroMapTool.build/Script-E964E3DE1FD640AF84633872.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/jingzhao/24783_project/rothberg-run/build0401/public/src/2DRetroMapTool/src/gui -f /Users/jingzhao/24783_project/rothberg-run/build0401/public/src/2DRetroMapTool/src/gui/CMakeScripts/2DRetroMapTool_postBuildPhase.make$CONFIGURATION all
