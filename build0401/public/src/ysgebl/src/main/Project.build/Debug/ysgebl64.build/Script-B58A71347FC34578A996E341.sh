bf21a5843e0afaf552a547d24c44fe6683047f97
ziliHarvey <zili@andrew.cmu.edu>
2019-04-12 12:34:57 -0400

    removed build

--- build0401/public/src/ysgebl/src/main/Project.build/Debug/ysgebl64.build/Script-B58A71347FC34578A996E341.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/jingzhao/24783_project/rothberg-run/build0401/public/src/ysgebl/src/main -f /Users/jingzhao/24783_project/rothberg-run/build0401/public/src/ysgebl/src/main/CMakeScripts/ysgebl64_postBuildPhase.make$CONFIGURATION all
--- build0401/public/src/ysgl/src/Project.build/Debug/ysgl.build/Script-D7D9ADAEDC59446389E02905.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/jingzhao/24783_project/rothberg-run/build0401/public/src/ysgl/src -f /Users/jingzhao/24783_project/rothberg-run/build0401/public/src/ysgl/src/CMakeScripts/ysgl_cmakeRulesBuildPhase.make$CONFIGURATION all
