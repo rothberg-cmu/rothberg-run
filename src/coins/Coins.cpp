19a18000f77417d372aa956eabd21d09abe0bd0b
Minjun Xu <minjunxu@andrew.cmu.edu>
2019-04-25 14:28:50 -0400

    add some functions for drawing and initializing

--- src/coins/Coins.cpp
+++ src/coins/Coins.cpp
@@ -1,8 +1,17 @@
 #include "Coins.h"
 
-Coin::Coin(YsVec3 p, int rid) {
+bool isNearCoin(Coin& coin, YsVec3& pos) {
+    return ((coin.getPos() - pos) * (coin.getPos() - pos)) < SURROUNDING_DISTANCE;
+}
+
+Coin::Coin(YsVec3 p, int rid, int o) {
     pos = p;
     roadIdx = rid;
+    ori = o;
+}
+
+void Coin::addSelfToDrawingVector(std::vector <float>& vtx, std::vector <float>& col, std::vector <float>& nom) {
+
 }
 
 YsVec3 Coin::getPos() {
@@ -14,5 +23,46 @@ int Coin::getRoadIdx() {
 }
 
 Coins::Coins(Map& m) : map(m) {
-    initCoins(map.getRoads().size());
+    initCoins();
+}
+
+std::vector <float>& Coins::getVtx() {
+    return vtx;
+}
+
+void Coins::initCoins() {
+    auto roads = map.getRoads();
+    for (int j = 0; j < roads.size(); j++) {
+        Road road = roads[j];
+        YsVec3 start = road.getRoadStart();
+        YsVec3 end = road.getRoadEnd();
+        bool o = road.getIsVertical();
+        for (int i = 0; i < COINS_ON_ONE_ROAD; i++) {
+            YsVec3 pos = start + (end - start) / ((float) COINS_ON_ONE_ROAD + 1) * ((float) (i + 1));
+            Coin coin(pos, j, o);
+            coins.push_back(coin);
+        }
+    }
+}
+
+void Coins::updateVtx(YsVec3& pos) {
+    int idx = -1;
+    for (int i = 0; i < coins.size(); i++) {
+        if (isNearCoin(coins[i], pos)) {
+            idx = i;
+        }
+    }
+    if (idx == -1) {
+        return;
+    }
+
+    std::swap(coins[idx], coins[coins.size()-1]);
+    coins.pop_back();
+    vtx.clear();
+    col.clear();
+    for (Coin coin : coins) {
+        vtx.push_back(coin.getPos().xf());
+        vtx.push_back(coin.getPos().yf());
+        vtx.push_back(coin.getPos().zf());
+    }
 }
--- src/coins/Coins.h
+++ src/coins/Coins.h
@@ -2,24 +2,34 @@
 #include <vector>
 #include "Map.h"
 
+#define SURROUNDING_DISTANCE 1
+#define COINS_ON_ONE_ROAD 3
+
 class Coin{
 public:
     int roadIdx = -1;
     YsVec3 pos;
-
+    int ori = -1; // 0 denotes not verticle and 1 denotes verticle road 
 public:
-    Coin(YsVec3 p, int rid);
+    Coin(YsVec3 p, int rid, int o);
     YsVec3 getPos();
     int getRoadIdx();
+    int getOrientation();
+    void addSelfToDrawingVector(std::vector <float>& vtx, std::vector <float>& col, std::vector <float>& nom);
 };
 
 class Coins {
 private:
-    void initCoins(int num);
+    void initCoins();
     Map& map;
+    std::vector <float> vtx;
+    std::vector <float> col;
+    std::vector <float> nom;
 public:
     Coins();
     Coins(int n);
     Coins(Map& m);
     std::vector <Coin> coins;
+    std::vector <float> & getVtx();
+    void updateVtx(YsVec3& pos);
 };
