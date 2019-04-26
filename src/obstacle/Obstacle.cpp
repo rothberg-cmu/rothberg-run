b05e546059ff971b092775973fac106b8065688c
Minjun Xu <minjunxu@andrew.cmu.edu>
2019-04-26 16:19:49 -0400

    finish basci functions for obstacles, need test

--- src/obstacle/Obstacle.cpp
+++ src/obstacle/Obstacle.cpp
@@ -1,9 +1,73 @@
 
 #include "Obstacle.h"
 
-Obstacle::Obstacle(Map& m) : map(m) {
+Obstacle::Obstacle(YsVec3& p, bool v, int rd) {
+    pos = p;
+    isVertical = v;
+    roadIdx = rd;
+}
+
+YsVec3 Obstacle::getPos() {
+    return pos;
+}
+
+bool Obstacle::getIsVertical() {
+    return isVertical;
+}
+
+Obstacles::Obstacles(Map& m) : map(m) {
     initObstacles();
 }
 
-void Obstacle::initObstacles() {
+
+void Obstacles::initObstacles() {
+    srand((unsigned)time(0));
+    obstacles.clear();
+    auto roads = map.getRoads();
+    for (int i = 0; i < roads.size(); i++) {
+        int randNum = std::rand() % 100;
+        if (randNum > OBSTACLES_PROBABILITY) {
+            continue;
+        }
+        int obstaclePos = std::rand() % 100;
+        float pro = obstaclePos / 100.0;
+        Road road = roads[i];
+        YsVec3 start = road.getRoadStart();
+        YsVec3 end = road.getRoadEnd();
+        YsVec3 p = start + (end - start) * pro;
+        obstacles.push_back(Obstacle(p, road.getIsVertical(), i));
+    }
+}
+
+void Obstacles::loadObstacleStl(const char* fileName) {
+    if (true == LoadBinStl(oneObstacleVtx, oneObstacleNom, fileName)) {
+        std::cout << "load obstacle stl successfully" << std::endl;
+        int s = obstacles.size();
+        for (int i = 0; i < s; i++) {
+            for (int j = 0; j < oneObstacleVtx.size() / 3; j++) {
+                vtx.push_back(oneObstacleVtx[3*j] + obstacles[i].getPos().xf());
+                vtx.push_back(oneObstacleVtx[3*j + 1] + obstacles[i].getPos().yf());
+                vtx.push_back(oneObstacleVtx[3*j + 1] + obstacles[i].getPos().zf());
+            }
+            for (int j = 0; j < oneObstacleVtx.size() / 3; j++) {
+                col.push_back(1);
+                col.push_back(0.3);
+                col.push_back(0.3);
+                col.push_back(1);
+            }
+        }
+    } else {
+        std::cout << "load obstacle stl fail" << std::endl;
+    }
+}
+
+void Obstacles::updateCount(YsVec3& pos) {
+}
+
+void Obstacles::drawObstacles(YsVec3& pos) {
+    updateCount(pos);
+
+    glVertexPointer(3,GL_FLOAT,0,vtx.data());
+    glColorPointer(4,GL_FLOAT,0,col.data());
+    glDrawArrays(GL_TRIANGLES,0,vtx.size()/3);
 }
--- src/obstacle/Obstacle.h
+++ src/obstacle/Obstacle.h
@@ -5,6 +5,10 @@
 
 #include "Map.h"
 #include <vector>
+#include <ctime>
+#include "binstl.h"
+#include <iostream>
+#include "fslazywindow.h"
 
 class Obstacle {
 private:
@@ -12,11 +16,19 @@ private:
     bool isVertical = false;
     int roadIdx = -1;
 public:
-    Obstacle(YsVec);
+    Obstacle(YsVec3& p, bool v, int rd);
+    YsVec3 getPos();
+    bool getIsVertical();
 };
 
 class Obstacles {
 private:
+    std::vector <Obstacle> obstacles;
+    std::vector <float> oneObstacleVtx;
+    std::vector <float> oneObstacleCol;
+    std::vector <float> oneObstacleNom;
+    std::vector <float> vtx;
+    std::vector <float> col;
     void initObstacles();
     Map& map;
     int count = 0;
@@ -24,6 +36,8 @@ public:
     Obstacles(Map& m);
     int getCount();
     void updateCount(YsVec3& pos);
+    void loadObstacleStl(const char* fileName);
+    void drawObstacles(YsVec3& pos);
 };
 
 
